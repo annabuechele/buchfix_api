6f08b5f5a78f5a376725ceb8947bb7fb50ade970
Patrick Cerny <patrick.cerny04@gmail.com>
2021-05-01 20:50:21 +0200

    ✨ Transaction based SQL insert at User

--- src/dataAccess/routes/user.ts
+++ src/dataAccess/routes/user.ts
@@ -26,71 +26,86 @@ router.post("/new", async (req: express.Request, res: express.Response) => {
   );
   if (!googleRes.data.success) return res.sendStatus(401);
 
-  //insert adress
-  const locationSQL =
-    "INSERT INTO address (street, number, zip, city, state, country) VALUES (?, ?, ?, ?, ?, ?)";
-  sql.query(
-    locationSQL,
-    [
-      user.location.street,
-      user.location.number,
-      user.location.zipCode,
-      user.location.city,
-      user.location.state,
-      user.location.country,
-    ],
-    async (
-      locationErr: mysql.MysqlError,
-      locationResults: any,
-      locationFields: mysql.FieldInfo[]
-    ) => {
-      if (locationErr) return res.sendStatus(500);
-
-      fk_address = locationResults.insertId;
-
-      //name insert
-      const nameSQL = "INSERT INTO name (firstname, lastname) VALUES (?, ?)";
-      sql.query(
-        nameSQL,
-        [user.name.firstName, user.name.lastName],
-        async (
-          nameErr: mysql.MysqlError,
-          nameResults: any,
-          nameFields: mysql.FieldInfo[]
-        ) => {
-          if (nameErr) return res.sendStatus(500);
-
-          fk_name = nameResults.insertId;
-          const salt = await require("crypto").randomBytes(16).toString("hex");
-
-          //user insert
-          const userSQL =
-            "INSERT INTO user (username, password, email, salt, is_admin, fk_name, fk_address) VALUES (?, Password(?), ?, ?, ?, ?, ?)";
-          sql.query(
-            userSQL,
-            [
-              user.username,
-              user.password + salt,
-              user.email,
-              salt,
-              0,
-              fk_name,
-              fk_address,
-            ],
-            async (
-              userErr: mysql.MysqlError,
-              userResults: any,
-              userFields: mysql.FieldInfo[]
-            ) => {
-              if (userErr) return res.sendStatus(500);
-
-              return res.sendStatus(200);
-            }
-          );
+  sql.beginTransaction((err: mysql.MysqlError) => {
+    //insert adress
+    const locationSQL =
+      "INSERT INTO address (street, number, zip, city, state, country) VALUES (?, ?, ?, ?, ?, ?)";
+    sql.query(
+      locationSQL,
+      [
+        user.location.street,
+        user.location.number,
+        user.location.zipCode,
+        user.location.city,
+        user.location.state,
+        user.location.country,
+      ],
+      async (
+        locationErr: mysql.MysqlError,
+        locationResults: any,
+        locationFields: mysql.FieldInfo[]
+      ) => {
+        if (locationErr) {
+          sql.rollback();
+          return res.sendStatus(500);
         }
-      );
-    }
-  );
+
+        fk_address = locationResults.insertId;
+
+        //name insert
+        const nameSQL = "INSERT INTO name (firstname, lastname) VALUES (?, ?)";
+        sql.query(
+          nameSQL,
+          [user.name.firstName, user.name.lastName],
+          async (
+            nameErr: mysql.MysqlError,
+            nameResults: any,
+            nameFields: mysql.FieldInfo[]
+          ) => {
+            if (nameErr) {
+              sql.rollback();
+              return res.sendStatus(500);
+            }
+
+            fk_name = nameResults.insertId;
+            const salt = await require("crypto")
+              .randomBytes(16)
+              .toString("hex");
+
+            //user insert
+            const userSQL =
+              "INSERT INTO user (username, password, email, salt, is_admin, fk_name, fk_address) VALUES (?, Password(?), ?, ?, ?, ?, ?)";
+            sql.query(
+              userSQL,
+              [
+                user.username,
+                user.password + salt,
+                user.email,
+                salt,
+                0,
+                fk_name,
+                fk_address,
+              ],
+              async (
+                userErr: mysql.MysqlError,
+                userResults: any,
+                userFields: mysql.FieldInfo[]
+              ) => {
+                if (userErr) {
+                  sql.rollback();
+                  return res.sendStatus(500);
+                }
+                sql.commit((commitError: mysql.MysqlError) => {
+                  if (err) return res.sendStatus(500);
+                  return res.sendStatus(200);
+                });
+              }
+            );
+          }
+        );
+      }
+    );
+  });
 });
 
 export default router;
