12041b7b30de99ad9e567650f7ace7eb3c985799
Patrick Cerny <patrick.cerny04@gmail.com>
2021-05-03 12:24:51 +0200

    ✨ Book img Inserts

--- src/dataAccess/routes/book.ts
+++ src/dataAccess/routes/book.ts
@@ -1,8 +1,52 @@
 import * as express from "express";
-
+import * as fs from "fs";
+import validateUser from "../../helpers/validateUser";
+import BookType from "../../types/bookType";
 const router = express.Router();
+import * as mysql from "mysql";
 
 //DB connection object
-const sql = require("../../helpers/sql_db");
+import sql from "../../helpers/sql_db";
+import { EROFS } from "node:constants";
+
+router.post("/new", (req: express.Request, res: express.Response) => {
+  try {
+    const isbn: string = req.body.book.isbn;
+    const base64IMGFull: string = req.body.base64;
+
+    const type: string = "." + base64IMGFull.substr(11, 3);
+
+    const base64Short: string = base64IMGFull.substr(22);
+
+    let buff = Buffer.from(base64Short, "base64");
+
+    fs.writeFileSync("../static/media/" + isbn + type, buff);
+
+    const insertNook: BookType = {
+      format: req.body.book.format,
+      genre: req.body.book.genre,
+      isbn: isbn,
+      path: "/static/media/",
+      file_name: +isbn + type,
+      sites: req.body.book.sites,
+      title: req.body.book.title,
+    };
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+
+  //   sql.beginTransaction((transactionErr: mysql.MysqlError) => {
+  //     if (transactionErr) return res.sendStatus(500);
+
+  //     const getGenreSQL: string = "SELCT id_genre FROM genre WHERE name = ?";
+  //     sql.query(
+  //       getGenreSQL,
+  //       [],
+  //       (getGenreError: mysql.MysqlError, genreResults: Array<any>) => {
+  //         if (genreResults.length === 0) return res.sendStatus(404);
+  //       }
+  //     );
+  //   });
+});
 
 export default router;
--- src/types/bookType.ts
+++ src/types/bookType.ts
@@ -1,5 +1,16 @@
 import { StringMappingType } from "typescript";
 
-type BookType = {}
+type BookType = {
+  isbn: string;
+  title: string;
+  sites: number;
+  fk_format?: number;
+  fk_genre?: number;
+  fk_file?: number;
+  genre: string;
+  format: string;
+  path: string;
+  file_name: string;
+};
 
 export default BookType;
