a324a840b24bc8e140f1970bd25c63736d02ca58
Anna Büchele <74052037+annabuechele@users.noreply.github.com>
2021-05-21 11:25:59 +0200

    login

--- src/helpers/validateUser.ts
+++ src/helpers/validateUser.ts
@@ -14,9 +14,11 @@ const validateUser = (
 ) => {
   const authHeader = req.headers["authorization"];
   const accessToken = authHeader && authHeader.split(" ")[1];
-
+  console.log(req.body, "a")
   if (accessToken == null)
     return res.status(401).send("Accesstoken not provided");
+
+
 
   jwt.verify(
     accessToken,
